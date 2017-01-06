c445a28e2e9025deab063cae8331147ca7b3845f
Jayanth Prathipati <jayanth@vt.edu>
2017-01-05 22:52:56 -0500

    added second portfolio piece

---
+++ jsons/markers.js
@@ -0,0 +1,20 @@
+var geoJsonFeatures = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    37.323010700000005,
+                    -122.03225190000001
+                ]
+            },
+            "properties": {
+                "job": "Amazon",
+                "language": "c"
+            }
+        }
+    ]
+};
